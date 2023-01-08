caccd14e2b35d65b61bdec4e35c7c507625b23a9
Siddharth Sharma <sharmasiddharthcs@gmail.com>
2023-01-08 00:45:02 -0800

    link

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import AuthForm from "@/components/layout/AuthForm";
 import { Box, Flex, Heading, Icon, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { MdCheckCircleOutline } from "react-icons/md";
@@ -11,9 +12,9 @@ const Login = () => {
     <Flex flex="1" align="center" justify="center">
       {router.query.verifyRequest ? (
         <Box mx={{ base: 4, md: 0 }} textAlign="center">
-          <Heading>
-            Head over to dashboard <a href="/dashboard">here</a>
-          </Heading>
+          <Link href="/dashboard">
+            <Heading>Head over to dashboard here</Heading>
+          </Link>
           {/* <Text mt={3} fontSize="2xl">
             A <b>sign in link</b> has been sent to your email address.
           </Text> */}
